2b7221abab56e7e7f398f2bd8dce6d19aebd7ea5
Rahuldeb5 <110701518+Rahuldeb5@users.noreply.github.com>
2023-07-21 14:37:29 -0400

    Update generate_summary.py

--- generate_summary.py
+++ generate_summary.py
@@ -17,5 +17,5 @@ def summarize_text(input_file, output_file):
 
 if __name__ == "__main__":
     input_file = "/home/nvidia/my_project/captured_text.txt"  # Replace with the path to your input text file
-    output_file = "text_summary.txt"  # Replace with the desired path for the output summary file
+    output_file = "/home/nvidia/my_project/text_summary.txt"  # Replace with the desired path for the output summary file
     summarize_text(input_file, output_file)
